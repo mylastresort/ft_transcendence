624095f051fb3707fcfc7d84b179910db0eab08e
Zakaria Kasmi <zakkikasmi@gmail.com>
2023-04-30 20:28:14 +0100

    frontend

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+function Xxx () {
+  return(
+    <h1>wefwefwefwefwef</h1>
+  )
+}
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+
+  return (
+    <div>
+
+    <Xxx/>
+      <Component {...pageProps} />
+    </div>
+  )
+
 }
