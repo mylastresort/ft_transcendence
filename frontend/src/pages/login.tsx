e98f3c3c158e3587f8092e350225f10ffdc79bb9
Zakaria Kasmi <zakkikasmi@gmail.com>
2023-07-03 17:19:55 +0100

    fix login dup

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { PostLogin, PostTokens, PostVerify2faTmp } from './api/auth/auth';
 import { Grid, Text, Spacer } from '@nextui-org/react';
@@ -17,15 +16,9 @@ const Styles = {
   },
 };
 
-function login({ setIsTwoFactorAuth }) {
-  setIsTwoFactorAuth(true);
-function login({ setIsTwoFactorAuth }) {
+function login({ setIsTwoFactorAuth }: any) {
   setIsTwoFactorAuth(true);
   const router = useRouter();
-  const [Is2fa, setIs2fa] = useState(false);
-  const [Code, setCode] = useState('');
-  const [isError, setIsError] = useState(false);
-
   const [Is2fa, setIs2fa] = useState(false);
   const [Code, setCode] = useState('');
   const [isError, setIsError] = useState(false);
@@ -33,7 +26,6 @@ function login({ setIsTwoFactorAuth }) {
   useEffect(() => {
     const QueryCode = window.location.search.split('code=')[1];
     if (QueryCode) {
-      const url = `/api/auth/callback?access_code=${QueryCode}`;
       const url = `/api/auth/callback?access_code=${QueryCode}`;
       PostTokens(url)
         .then((res) => {
@@ -62,7 +54,7 @@ function login({ setIsTwoFactorAuth }) {
     }
   }, []);
 
-  const HandleCode = (e) => {
+  const HandleCode = (e: any) => {
     if (e.target.value.length === 6 && e.target.value.match(/^[0-9]+$/)) {
       setCode(e.target.value);
       const data = {
@@ -115,5 +107,4 @@ function login({ setIsTwoFactorAuth }) {
   );
 }
 
-export default withAuth(login);
 export default withAuth(login);
