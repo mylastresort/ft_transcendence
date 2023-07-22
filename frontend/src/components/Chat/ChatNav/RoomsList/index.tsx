2393dfd6a60886b708b367f76f5dd3ca0f8c524a
Mohammed Benkhattab <mobenkhattab@gmail.com>
2023-07-22 09:22:54 +0100

    tmp push2

--- frontend/src/components/Chat/ChatNav/RoomsList/index.tsx
+++ frontend/src/components/Chat/ChatNav/RoomsList/index.tsx
@@ -14,10 +14,18 @@ import { useContext, useEffect, useState } from 'react';
 import { UserCard } from './UserCard';
 import request from 'superagent';
 
+interface ChannelData {
+  ChannelPassword: null;
+  channelName: 'The 1337 party';
+  createdAt: '2023-07-22T07:25:02.824Z';
+  description: 'this is my first created chat';
+  id: 2;
+  image: 'https://iintra.freekb.es/banners/mbenkhat-1670055933.jpeg';
+  ownerId: 13;
+  updateAt: '2023-07-22T07:25:02.824Z';
+}
 
-
-
-function ChannelCard({ user, closeNav }: any) {
+function ChannelCard({ channel, closeNav }: {channel: ChannelData, closeNav: any}) {
   const [bgColor, setBgColor] = useState('var(--white-color)');
   return (
     <Link href={'/chat/channels'}>
@@ -25,7 +33,7 @@ function ChannelCard({ user, closeNav }: any) {
         maw={300}
         mx="auto"
         onMouseOver={() => {
-          setBgColor('red');
+          setBgColor('var(--chat-red-color)');
         }}
         onMouseLeave={() => {
           setBgColor('var(--white-color)');
@@ -35,22 +43,23 @@ function ChannelCard({ user, closeNav }: any) {
           backgroundColor: bgColor,
           borderRadius: '10px',
           border: '2px solid',
-          borderColor: 'red',
+          borderColor: 'var(--chat-red-color)',
           padding: '10px',
           margin: '15px auto',
           marginTop: '0px',
         }}
+        onClick={closeNav}
       >
         <Group noWrap h={60}>
           <Avatar
             radius={50}
             size={60}
-            src={'https://cdn-icons-png.flaticon.com/512/3772/3772059.png'}
+            src={channel.image}
           />
           <div>
-            <Text size="lg">{user.name}</Text>
+            <Text size="lg">{channel.channelName}</Text>
             <Text size="xs" color="dimmed">
-              user.Messages[0].content
+              this is last message
             </Text>
           </div>
         </Group>
@@ -60,28 +69,49 @@ function ChannelCard({ user, closeNav }: any) {
 }
 
 export function RoomsList({ closeNav }: any) {
-  const [PrivateChats, setPrivateChats] = useState([]);
-  useEffect(() => {
-    const jwtToken = localStorage.getItem('jwtToken');
+  const jwtToken = localStorage.getItem('jwtToken');
+  const [chatList, setchatList] = useState([]);
+  // const [channels, setchannels] = useState([]);
+  // useEffect(() => {
+    // }, []);
+    useEffect(() => {
+      request
+        .get('http://localhost:4400/api/chat/private')
+        .set('Authorization', `Bearer ${jwtToken}`)
+        .then((res) => setchatList(res.body))
+        .catch((err) => {
+          return err;
+        });
     request
-      .get('http://localhost:4400/api/chat/private')
+      .get('http://localhost:4400/api/chat/channel')
       .set('Authorization', `Bearer ${jwtToken}`)
-      .then((res) => setPrivateChats(res.body))
+      .then((res) => setchatList(chatList.concat(res.body)))
       .catch((err) => {
         return err;
       });
   }, []);
 
   return (
-    <ScrollArea h={'calc(100% - 200px)'} mt={20}>
-      {PrivateChats.map((room: any) => (
-        <>
-          <UserCard
-            user={room}
-            closeNav={closeNav}
-          />
-        </>
-      ))}
-    </ScrollArea>
+    // <ScrollArea h={'calc(100% - 200px)'} mt={20}>
+    //   {PrivateChats.map((room: any) => (
+    //     <>
+    //       <UserCard
+    //         user={room}
+    //         closeNav={closeNav}
+    //       />
+    //     </>
+    //   ))}
+    // </ScrollArea>
+    // <ScrollArea h={'calc(100% - 200px)'} mt={20}>
+    //   {chatList.map((chat: any) => (
+    //     <>
+    //       <ChannelCard channel={chat} closeNav={closeNav} />
+    //     </>
+    //   ))}
+    // </ScrollArea>
+    <Group>
+      <Button>channels</Button>
+      <Button>channels</Button>
+    </Group>
   );
 }
