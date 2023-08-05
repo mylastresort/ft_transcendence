5230da3e9033e3e5f549e048831887575b10ec8a
Mohammed Benkhattab <mobenkhattab@gmail.com>
2023-08-05 11:42:54 +0100

    add invite member to game from chat

---
+++ backend/src/chat/private/privateChat.gateway.ts
@@ -0,0 +1,57 @@
+import { OnModuleInit, UseGuards } from '@nestjs/common';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { WsJwtGuard } from 'src/auth/ws-jwt/ws-jwt.guard';
+import { PrivateChatService } from './privateChat.service';
+
+@WebSocketGateway({
+  namespace: 'chat',
+  cors: {
+    origin: 'http://localhost:3000',
+  },
+})
+@UseGuards(WsJwtGuard)
+export class PrivateChatGatway {
+  constructor(private privateChatService: PrivateChatService) {}
+  @WebSocketServer()
+  server: Server;
+
+  @UseGuards(WsJwtGuard)
+  @SubscribeMessage('private/join-room')
+  joinPrivateRoom(
+    @MessageBody() room: any,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    console.log('joinPrivateRoom: ', room);
+    client.join(room);
+  }
+
+  @UseGuards(WsJwtGuard)
+  @SubscribeMessage('private/leave-room')
+  leavePrivateRoom(
+    @MessageBody() room: any,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    console.log('leavePrivateRoom: ', room);
+    client.leave(room);
+  }
+
+  @SubscribeMessage('private/sendMsg')
+  async SendPrivateMessage(
+    @MessageBody() data: any,
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log('SendPrivateMessage: ', data);
+    const createdMessage = await this.privateChatService.createMessage(
+      client.data,
+      data,
+    );
+    this.server.to(data.id).emit('private/newMsg', createdMessage);
+  }
+}
--- frontend/src/components/Chat/ChatRoom/UserInfo/index.tsx
+++ frontend/src/components/Chat/ChatRoom/UserInfo/index.tsx
@@ -9,15 +9,26 @@ import {
   useMantineTheme,
 } from '@mantine/core';
 import Link from 'next/link';
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import request from 'superagent';
 
+interface UserInfo{
+  id: number;
+  firstName: string;
+  lastName: string;
+  sammary: string;
+  status: string;
+}
+
 function UserInfo() {
+  const jwtToken = localStorage.getItem('jwtToken');
   const chatContext = useContext(ChatContext);
+  const userContext = useContext(UserContext);
   const theme = useMantineTheme();
+  const [userInfo, setUserInfo] = useState({} as UserInfo);
   console.log(chatContext.data);
+  const online = userInfo.status == 'online'
   function deleteUser() {
-    const jwtToken = localStorage.getItem('jwtToken');
     request
       .post('http://localhost:4400/api/chat/private/delete')
       .set('Authorization', `Bearer ${jwtToken}`)
@@ -27,47 +38,70 @@ function UserInfo() {
       });
   }
 
-  useEffect(()=>{
-    
-  }, [])
+  useEffect(() => {
+    request
+      .get(`http://localhost:4400/api/chat/user/${chatContext.data.name}`)
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .then((res) => {
+        setUserInfo(res.body);
+        console.log('user Info: ', res.body);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <Box w={'100%'} h={'100%'} bg={theme.colors.dark[6]} pt={100}>
-      <Flex direction={'column'} gap={10} style={{
-        border: '1px solid var(--secondary-color)',
-        borderRadius: '20px'
-      }}>
+      <Flex direction={'column'} gap={10}>
         <Avatar
           m={'auto'}
           radius={500}
           size={150}
           style={{
-            border: '3px solid',
-            borderColor: theme.colors.dark[1],
+            border: '4px solid',
+            borderColor: online ? 'green' : 'red',
           }}
           src={chatContext.data.img}
         />
         <Text fw={500} size={'xl'} m={'auto'}>
           {chatContext.data.name}
         </Text>
-        <Link href={'/chat'} style={{
-          margin:'auto'
-        }}>
+        <Box
+         w={'90%'}
+         m={'auto'}
+         mih={100}
+         p={10}
+          style={{
+            border: '1px solid var(--secondary-color)',
+            borderRadius: '20px',
+          }}
+        >
+          <Text>
+            {userInfo.sammary}
+          </Text>
+        </Box>
+        <Link
+          href={'/chat'}
+          style={{
+            margin: 'auto',
+          }}
+        >
           <Link href={`/profile/${chatContext.data.name}`}>
-          <Button color="blue">
-            View Profile
-          </Button>
+            <Button color="blue">View Profile</Button>
+          </Link>
+          <Link href={`/game/invite?userId=${userInfo.id}`}>
+            <Button color="blue">Invite to Play</Button>
           </Link>
           <Button
             color="red"
-            
             w={300}
             onClick={() => {
               deleteUser();
             }}
-            >
+          >
             delete conversation
           </Button>
-          </Link>
+        </Link>
       </Flex>
     </Box>
   );
