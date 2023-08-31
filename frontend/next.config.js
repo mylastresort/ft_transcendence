5c98bc28194f4384ec53a69bcfcfab395f192ec2
mylastresort <stamim@student.1337.ma>
2023-08-31 18:31:19 +0000

    adding 42 digitalocean credentials

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -15,7 +15,11 @@ const nextConfig = {
   },
   env: {
     FRONTEND_DOMAIN: 'http://137.184.7.150:3000',
-    BACKEND_DOMAIN: 'http://137.184.7.150:4400'
+    BACKEND_DOMAIN: 'http://137.184.7.150:4400',
+    FORTYTWO_CLIENT_ID:
+      'u-s4t2ud-5bb57665a5f45d59c6ca9a2d310d6a1a17052426b99d6f59c371a3a696daa8c8',
+    FORTYTWO_CLIENT_SECRET:
+      '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',
   }
 };
 
