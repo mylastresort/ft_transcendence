e0961bf8e1eaf7004751581372e0c6c7c6116419
mylastresort <stamim@student.1337.ma>
2023-09-01 22:24:10 +0100

    changing the address

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,13 +9,13 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://10.13.1.15:3000/api/:path*',
+        destination: 'http://137.184.7.150:3000/api/:path*',
       },
     ];
   },
   env: {
-    FRONTEND_DOMAIN: 'http://10.13.1.15:5173',
-    BACKEND_DOMAIN: 'http://10.13.1.15:3000',
+    FRONTEND_DOMAIN: 'http://137.184.7.150:5173',
+    BACKEND_DOMAIN: 'http://137.184.7.150:3000',
     FORTYTWO_CLIENT_ID:
       'u-s4t2ud-5980f22b8a44c6b6f182b3c4ae059cb1abdad774f4fe24988c70d81374caaa44',
     FORTYTWO_CLIENT_SECRET:
