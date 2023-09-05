26e066e72452728e80dc58b10dd61f58a4469512
mylastresort <stamim@student.1337.ma>
2023-09-05 12:36:00 +0100

    removing watchers on leave

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,13 +9,15 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:4400/api/:path*',
+        destination: 'http://10.13.2.7:3000/api/:path*',
       },
     ];
   },
   env: {
-    FRONTEND_DOMAIN:'http://localhost:3000',
-    BACKEND_DOMAIN:'http://localhost:4400'
+    FRONTEND_DOMAIN: 'http://10.13.2.7:5173',
+    BACKEND_DOMAIN: 'http://10.13.2.7:3000',
+    FORTYTWO_CLIENT_ID: "u-s4t2ud-30e8e768b5e6a93c2075ffca4693f8f6b7b9583992f5b0cf97d284417b61f394",
+    FORTYTWO_CLIENT_SECRET: "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",
   }
 };
 
